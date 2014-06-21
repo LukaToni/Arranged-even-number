8bf55e8aed81238cd62f95820d56ea6893681556
Luka <luka.toni93@gmail.com>
2014-06-21 14:05:10 +0200

    Update HW-1.c

--- HW-1.c
+++ HW-1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//swap by pointer
 void swap (int *a, int *b)
 {
 	int c = *a;
@@ -12,24 +13,32 @@ int main ()
 {
 	int n, i;
 	scanf ("%d", &n);
-	int *tabela = malloc (n * sizeof (int));
+	int *table = malloc (sizeof (int) * n);
+	
+	//insert number in table
 	for (i = 0; i < n; ++i)
-		scanf ("%d", &tabela[i]);
+		scanf ("%d", &table[i]);
+		
 	int a, b , swaps;
+	
+	//bubble sort
 	for (;;)
 	{
 		swaps = 0;
 		for (a = 0; a < n; ++a)
 		{
-			if (tabela[a] % 2 == 0)
-			{
+			//if it is even number
+			if (table[a] % 2 == 0)
+			{	
+				//looking for next even number
 				for (b = a + 1; b < n; ++b)
 				{
-					if (tabela[b] % 2 == 0)
+					if (table[b] % 2 == 0)
 					{
-						if (tabela[a] > tabela[b])
+						//swap an even number
+						if (table[a] > table[b])
 						{
-							swap (&tabela[a], &tabela[b]);
+							swap (&table[a], &table[b]);
 							++swaps;
 						}
 						a = b;
@@ -41,7 +50,10 @@ int main ()
 		if (swaps == 0)
 			break;	
 	}
+	
 	for (i = 0; i < n ; ++i)
-		printf ("%d ", tabela[i]);
+		printf ("%d ", table[i]);
 	printf ("\n");
+	
+	return 0;
 }
